daccfef01efc523800aacb4d58ee4d5530975b8c
Francesco Forte <francesco.forte831tech@gmail.com>
2023-06-07 19:06:35 -0400

    Initial commit

---
+++ src/test/java/io/francescof831/stock/StockNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package io.francescof831.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
